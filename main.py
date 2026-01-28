d444b872c72b90ba0a5d294b045424aa4dd6020c
orphankings <161202381+orphankings@users.noreply.github.com>
2026-01-28 23:35:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__) # 架設伺服器
 
+if __name__ == "__main__":
+    # Render 會提供 PORT 環境變數
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
 def load_players():
     try:
         with open("data/players.json", "r", encoding="utf-8") as f:
